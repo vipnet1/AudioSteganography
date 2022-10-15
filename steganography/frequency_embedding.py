249defa4f70ded03dba517ab4d65046946a88f37
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2022-10-15 21:53:46 +0300

    feat: .

--- steganography/frequency_embedding.py
+++ steganography/frequency_embedding.py
@@ -7,6 +7,13 @@ import core.common as common
 def hide(filename):
     message = arguments_parser.parse_message()
 
+    if '#' in message:
+        print('# is special character, do not use it in message!')
+        return
+
+    message += '#'
+    message_bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in message])))
+
     file = WaveFile(filename)
 
     change_sign_samples = int((file.SampleRate / constants.FREQUENCY_TO_EMBED) / 2)
@@ -22,7 +29,9 @@ def hide(filename):
 
     new_data = []
 
+    message_index = 0
     data_index = 0
+    finished_hidding = False
     while data_index < len(file.data):
         can_embed = True
 
@@ -40,8 +49,43 @@ def hide(filename):
             new_data.append(file.data[data_index])
             data_index += 1
 
-        if not can_embed:
+            channels_positivity[channel] = positivity
+
+        if finished_hidding or not can_embed:
             continue
+
+        for samples_to_swap in [change_sign_samples - 1, change_sign_samples, 1]:
+            for _ in range(samples_to_swap):
+                for channel_num in range(file.NumChannels):
+                    for _ in range(bytes_per_sample - 1):
+                        if finished_hidding:
+                            byte_data = int('0'*8, 2)
+                            new_data.append(byte_data)
+                            continue
+
+                        byte_data = int("".join(str(bit) for bit in message_bits[message_index : message_index + 8]).ljust(8,'0'), 2)
+                        new_data.append(byte_data)
+                        message_index += 8
+
+                        if message_index >= len(message_bits): finished_hidding = True
+
+                    if finished_hidding:
+                        byte_data = int('0' if channels_positivity[channel_num] == Positivity.POSITIVE else '1' + '0'*7, 2)
+                        new_data.append(byte_data)
+                        continue
+
+                    byte_data = int('0' if channels_positivity[channel_num] == Positivity.POSITIVE else '1' + "".join(str(bit) for bit in message_bits[message_index : message_index + 7]).ljust(7,'0'), 2)
+                    new_data.append(byte_data)
+                    message_index += 7
+            
+            # switch positivity
+            channels_positivity[channel_num] =  Positivity.NEGATIVE if channels_positivity[channel_num] == Positivity.POSITIVE else Positivity.NEGATIVE
+
+            
+
+            
+
+        
 
     file.replace_data_block(new_data)
 
